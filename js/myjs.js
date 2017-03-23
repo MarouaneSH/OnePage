7c25b2122e74844d6c86e19e800dd0b89a82193f
Marouane sh <marouanesouah@gmail.com>
2017-03-22 17:55:16 -0700

    Ba9a 3 Section

--- js/myjs.js
+++ js/myjs.js
@@ -20,6 +20,62 @@ $(window).on('resize', function (){
 });
 
 $('.carousel').carousel({
-  interval: 66000,
+  interval: 686000,
   pause: "true"
 });
+
+
+
+  $(document).ready(function(){
+        
+         $(".disable-img").eq(0).click(function(){
+             ShowServices();
+             $(this).css("opacity","0");
+             $(".development").slideDown("slow");
+         })
+
+         $(".disable-img").eq(1).click(function(){
+             ShowServices();
+             $(this).css("opacity","0");
+             $(".development").hide();
+         })
+
+         $(".disable-img").eq(2).click(function(){
+             ShowServices();
+             $(this).css("opacity","0");
+             $(".development").hide();
+         })
+
+         $(".disable-img").eq(3).click(function(){
+             ShowServices();
+             $(this).css("opacity","0");
+             $(".development").hide();
+         })
+          
+     })
+
+
+     var display = false;
+     function ShowServices(){
+     
+            if(!display)
+           {
+                $(".img-service ").animate({
+                    "height":"80px"
+                })
+                  $(".img-service img").animate({
+                    "width":"100px"
+                })
+                $(".offre-tarifs h3").animate({
+                    "font-size":"15px"
+                })
+                
+                $(".img-service").parent().children(".disable-img").css("opacity","0.7");
+                $(".offre-tarifs").css('backgroundColor',"white")
+                display=true;
+             } 
+             else
+             {
+                $(".img-service").parent().children(".disable-img").css("opacity","0.7");
+             }    
+     }
